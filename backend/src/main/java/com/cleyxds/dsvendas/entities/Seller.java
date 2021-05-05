d587083ef0f86af625eb673c374ec6ec498e9714
cleyxds <cleysonn@hotmail.com.br>
2021-05-05 13:10:56 -0300

    Domain model, database seed

---
+++ backend/src/main/java/com/cleyxds/dsvendas/entities/Seller.java
@@ -0,0 +1,81 @@
+package com.cleyxds.dsvendas.entities;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_sellers")
+public class Seller {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private String name;
+
+  @OneToMany(mappedBy = "seller")
+  private List<Sale> sales;
+
+  public Seller() {}
+
+  public Seller(Long id, String name) {
+    this.id = id;
+    this.name = name;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public List<Sale> getSales() {
+    return sales;
+  }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((id == null) ? 0 : id.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    Seller other = (Seller) obj;
+    if (id == null) {
+      if (other.id != null)
+        return false;
+    } else if (!id.equals(other.id))
+      return false;
+    return true;
+  }
+
+  @Override
+  public String toString() {
+    return "Seller [id=" + id + ", name=" + name + "]";
+  }
+
+}
